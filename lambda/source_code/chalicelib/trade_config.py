80e2a6e03067cd622e80f423e25325a54fd768b8
NobitaInLove <121101145+NobitaInLove@users.noreply.github.com>
2023-06-23 17:03:39 +0530

    vinayak info
    Signed-off-by: NobitaInLove <121101145+NobitaInLove@users.noreply.github.com>

--- lambda/source_code/chalicelib/trade_config.py
+++ lambda/source_code/chalicelib/trade_config.py
@@ -5,23 +5,36 @@ tradingview_alert_passphrase = 'happytrading12378910'
 
 # kotak configuration
 kotak_config = {
-    "host" : "https://tradeapi.kotaksecurities.com/apim",
-    "instrument_token_script" :  "https://tradeapi.kotaksecurities.com/apim/scripmaster/1.1/filename",
-    "user_config" : [
-            {
-            "user_enabled" : "y", # user enabled Yes / No
-            "risk_per_trade_percentage" : 1,
-            "access_token":"be233436-b6f3-3b63-8b17-586c0b264d3f",
-            "user_name":"abhay.tiwari",
-            "userid":"AT12121987",
-            "kotak_password" : "anvi@2211",
-            "clientCode" : "YPS12",
-            "consumer_key":"aN4r6MSud8MOxpQ7_Ta17bK5gsca",
-            "consumer_secret":"tpMiMU784qvqjp464f4BTvF1UEsa",
-            "app_id":"DefaultApplication",
-            "telegram_chatid" : "",
-            "email_id" : "itsabhaytiwari2020@gmail.com"
-            } 
-            # add more users here
-        ]
+    "host": "https://tradeapi.kotaksecurities.com/apim",
+    "instrument_token_script":  "https://tradeapi.kotaksecurities.com/apim/scripmaster/1.1/filename",
+    "user_config": [
+        {
+            "user_enabled": "y",  # user enabled Yes / No
+            "risk_per_trade_percentage": 1,
+            "access_token": "be233436-b6f3-3b63-8b17-586c0b264d3f",
+            "user_name": "abhay.tiwari",
+            "userid": "AT12121987",
+            "kotak_password": "miku@2211",
+            "clientCode": "YPS12",
+            "consumer_key": "aN4r6MSud8MOxpQ7_Ta17bK5gsca",
+            "consumer_secret": "tpMiMU784qvqjp464f4BTvF1UEsa",
+            "app_id": "DefaultApplication",
+            "telegram_chatid": "-786074913",
+            "email_id": "itsabhaytiwari2020@gmail.com"
+        },
+        {
+            "user_enabled": "y",  # user enabled Yes / No
+            "risk_per_trade_percentage": 1,
+            "access_token": "30cb3bb1-0ece-3a31-9114-5e793b6a44f8",
+            "user_name": "vinayak.gupta",
+            "userid": "VG18091980",
+            "kotak_password": "Darshan@9",
+            "clientCode": "YPRRW",
+            "consumer_key": "M2A_cvNHd0a0nDOVnIjID_S41Hca",
+            "consumer_secret": "j2VkAyu_ZPfg4j3ahxXseSXsnnMa",
+            "app_id": "DefaultApplication",
+            "telegram_chatid": "-854770801",
+            "email_id": "guptavinayak@gmail.com"
+        }
+    ]
 }
